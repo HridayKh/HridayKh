4c0c6acad925d1d2b5a98d7ea2af8ba7248fc949
HridayDev <hridaykh1234@gmail.com>
2025-03-22 10:19:02 +0530

    add blogs, awards, certificaton, minor tweaks

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { MapPin, Mail, Linkedin, NotebookText, Github } from "lucide-react";
+import { MapPin, Mail, Linkedin, NotebookText, Github, ChevronDown, ChevronRight } from "lucide-react";
 import React, { useState } from "react";
 
 export default function App() {
@@ -8,12 +8,12 @@ export default function App() {
 				<div className="col left-col">
 					<Links />
 					<Experiences />
-					<div>Other sections...</div>
+					<Achieve />
 				</div>
 				<div className="col right-col">
 					<AboutMe />
 					<Projects />
-					<div>Blogs...</div>
+					<Blogs />
 				</div>
 			</div>
 		</>
@@ -25,19 +25,19 @@ function Links() {
 		<div>
 			<ul>
 				<li>
-					<a href="mailo:hridaykh@gmail.com" class="link"> <Mail size={"1em"} /> hridaykh@gmail.com </a>
+					<a href="mailo:hridaykh@gmail.com" className="link"> <Mail size={"1em"} /> hridaykh@gmail.com </a>
 				</li>
 				<li>
-					<a href="#" class="link"> <Linkedin size={"1em"} /> LinkedIn </a>
+					<a href="#" className="link"> <Linkedin size={"1em"} /> LinkedIn </a>
 				</li>
 				<li>
-					<a href="#" class="link"> <Github size={"1em"} /> GitHub </a>
+					<a href="#" className="link"> <Github size={"1em"} /> GitHub </a>
 				</li>
 				<li>
-					<a href="#" class="link"> <NotebookText size={"1em"} /> Blogs </a>
+					<a href="#" className="link"> <NotebookText size={"1em"} /> Blogs </a>
 				</li>
 				<li>
-					<a href="#" class="link"> <MapPin size={"1em"} /> India </a>
+					<a href="#" className="link"> <MapPin size={"1em"} /> India </a>
 				</li>
 			</ul>
 		</div>
@@ -51,7 +51,7 @@ function AboutMe() {
 		<h6 style={{ textDecoration: "none", color: "var(--scd)" }}> Aspiring Software Engineer | UGEE Aspirant | Open to Internships </h6>
 		<hr />
 
-		<p style={{ color: "var(--text)", fontSize: "0.75em", fontFamily: "Trebuchet MS, sans-serif" }}>
+		<p className="text">
 			This portfolio highlights my journey in software development, focusing on coding best practices, algorithms, and system design. My work emphasizes problem-solving, hands-on learning, and building efficient solutions from concept to implementation. I'm eager to apply my skills through internships and collaborations. Let's innovate together!
 		</p>
 
@@ -73,7 +73,16 @@ function Experiences() {
 				style={{ textDecoration: "none", color: "var(--prm)", cursor: "pointer" }}
 				onClick={toggleDropdown}
 			>
-				{isOpen ? "▼ Experience" : "► Experience"}
+				{isOpen ? (
+					<>
+						<ChevronDown size="1em" style={{ paddingTop: "0.3em" }} />Experience
+					</>
+				) : (
+					<>
+						<ChevronRight size="1em" style={{ paddingTop: "0.3em" }} />Experience
+					</>
+				)}
+
 			</h1>
 
 			{/* Dropdown Content */}
@@ -86,13 +95,7 @@ function Experiences() {
 							<h6 style={{ color: "var(--scd)" }}>
 								Tech Company XYZ | May 2023 - Aug 2023
 							</h6>
-							<p
-								style={{
-									color: "var(--text)",
-									fontSize: "0.75em",
-									fontFamily: "Trebuchet MS, sans-serif",
-								}}
-							>
+							<p className="text">
 								- Developed and optimized React components for a real-time
 								dashboard.
 								<br />
@@ -107,13 +110,7 @@ function Experiences() {
 							<h6 style={{ color: "var(--scd)" }}>
 								Self-Employed | Jan 2023 - Apr 2023
 							</h6>
-							<p
-								style={{
-									color: "var(--text)",
-									fontSize: "0.75em",
-									fontFamily: "Trebuchet MS, sans-serif",
-								}}
-							>
+							<p className="text">
 								- Designed and deployed responsive websites for small businesses.
 								<br />
 								- Integrated payment gateways and third-party services.
@@ -127,13 +124,7 @@ function Experiences() {
 							<h6 style={{ color: "var(--scd)" }}>
 								Various Projects | 2022 - Present
 							</h6>
-							<p
-								style={{
-									color: "var(--text)",
-									fontSize: "0.75em",
-									fontFamily: "Trebuchet MS, sans-serif",
-								}}
-							>
+							<p className="text">
 								- Contributed to open-source projects, focusing on frontend and
 								documentation.
 								<br />
@@ -163,7 +154,15 @@ function Projects() {
 				style={{ textDecoration: "none", color: "var(--prm)", cursor: "pointer" }}
 				onClick={toggleDropdown}
 			>
-				{isOpen ? "▼ Projects" : "► Projects"}
+				{isOpen ? (
+					<>
+						<ChevronDown size="1em" style={{ paddingTop: "0.3em" }} />Projects
+					</>
+				) : (
+					<>
+						<ChevronRight size="1em" style={{ paddingTop: "0.3em" }} />Projects
+					</>
+				)}
 			</h1>
 
 			{/* Dropdown Content */}
@@ -187,10 +186,12 @@ function Projects() {
 								<h3 style={{ textDecoration: "none", color: "var(--prm)" }}> Project Title 1 </h3>
 								<h6 style={{ textDecoration: "none", color: "var(--scd)", marginBottom: "0.5em" }}> A brief one-line description of the project. </h6>
 								<br />
-								<a href="#" class="link" style={{ fontSize: "1em" }}> <Github size={"0.8em"} /> GitHub Link Here </a>
+								<a href="#" className="link" style={{ fontSize: "1em" }}> <Github size={"0.8em"} /> GitHub Link Here </a>
 							</div>
 						</div>
+
 						{/* Project 2 */}
+						<hr style={{ width: "90%", borderTop: "1px dashed var(--acc)" }} />
 						<div style={{ marginBottom: "1.5em", display: "flex", gap: "1em" }}>
 							<img
 								src="https://avatars.githubusercontent.com/u/93050582?v=4" // Replace with your project image URL
@@ -206,10 +207,12 @@ function Projects() {
 								<h3 style={{ textDecoration: "none", color: "var(--prm)" }}> Project Title 2 </h3>
 								<h6 style={{ textDecoration: "none", color: "var(--scd)", marginBottom: "0.5em" }}> A brief one-line description of the project. </h6>
 								<br />
-								<a href="#" class="link" style={{ fontSize: "1em" }}> <Github size={"0.8em"} /> GitHub Link Here </a>
+								<a href="#" className="link" style={{ fontSize: "1em" }}> <Github size={"0.8em"} /> GitHub Link Here </a>
 							</div>
 						</div>
+
 						{/* Project 3 */}
+						<hr style={{ width: "90%", borderTop: "1px dashed var(--acc)" }} />
 						<div style={{ marginBottom: "1.5em", display: "flex", gap: "1em" }}>
 							<img
 								src="https://avatars.githubusercontent.com/u/93050582?v=4" // Replace with your project image URL
@@ -225,7 +228,7 @@ function Projects() {
 								<h3 style={{ textDecoration: "none", color: "var(--prm)" }}> Project Title 3 </h3>
 								<h6 style={{ textDecoration: "none", color: "var(--scd)", marginBottom: "0.5em" }}> A brief one-line description of the project. </h6>
 								<br />
-								<a href="#" class="link" style={{ fontSize: "1em" }}> <Github size={"0.8em"} /> GitHub Link Here </a>
+								<a href="#" className="link" style={{ fontSize: "1em" }}> <Github size={"0.8em"} /> GitHub Link Here </a>
 							</div>
 						</div>
 					</div>
@@ -235,3 +238,188 @@ function Projects() {
 	);
 }
 
+function Achieve() {
+	return (<><div>
+		<Awards />
+		<Certifications />
+	</div></>);
+}
+
+function Awards() {
+	return (
+		<>
+			<h1 style={{ textDecoration: "none", color: "var(--prm)" }}> Awards </h1>
+
+			<hr />
+			<div>
+				<Award
+					title="2nd Place - ICSO"
+					organization="Science Olympiad Foundation"
+					year="2024"
+					description={[
+						"Secured 2nd place in the International Computer Science Olympiad.",
+						"Demonstrated excellent analytical and problem-solving skills in computer science.",
+					]}
+				/>
+				<Award
+					title="1st Place - Python Coding Competition"
+					organization="Spring Dales"
+					year="2024"
+					description={[
+						"Won 1st place in an inter-school coding competition.",
+						"Built 2 python applications to demonstrate advanced python skills.",
+					]}
+				/>
+				<Award
+					title="3rd Place - Intercity python Competition"
+					organization="DCM Group of Schools"
+					year="2024"
+					description={[
+						"Achieved 3rd place in a challenging intercity competition involving teamwork and innovation.",
+						"Built 2 interactive python applications to solve problems.",
+					]}
+				/>
+			</div>
+		</>
+	);
+}
+
+function Certifications() {
+	return (
+		<>
+			<h1 style={{ textDecoration: "none", color: "var(--prm)" }}> Certifications </h1>
+
+			<hr />
+			<div>
+				<Award
+					title="Example Certification"
+					organization="lorem ipsm doto sit amet"
+					year="20xx"
+					description={[
+						"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+						"Nullam dignissim velit eu nulla feugiat, in gravida nunc faucibus.",
+					]}
+				/>
+			</div>
+		</>
+	);
+}
+
+// Award Component
+function Award({ title, organization, year, description }) {
+	return (
+		<div style={{ marginBottom: "1.5em" }}>
+			<h4 style={{ color: "var(--prm)" }}>{title}</h4>
+			<h6 style={{ color: "var(--scd)" }}>
+				{organization} | {year}
+			</h6>
+			<p className="text">
+				{description.map((item, index) => (
+					<>
+						{item}
+						<br />
+					</>
+				))}
+			</p>
+		</div>
+	);
+}
+
+function Blogs() {
+	const [isOpen, setIsOpen] = useState(true);
+
+	const toggleDropdown = () => {
+		setIsOpen(!isOpen);
+	};
+
+	return (
+		<div>
+			{/* Dropdown Header */}
+			<h1
+				style={{ textDecoration: "none", color: "var(--prm)", cursor: "pointer" }}
+				onClick={toggleDropdown}
+			>
+				{isOpen ? (
+					<>
+						<ChevronDown size="1em" style={{ paddingTop: "0.3em" }} />Blogs
+					</>
+				) : (
+					<>
+						<ChevronRight size="1em" style={{ paddingTop: "0.3em" }} />Blogs
+					</>
+				)}
+			</h1>
+
+			{/* Dropdown Content */}
+			{isOpen && (
+				<>
+					<hr />
+					<div>
+						{/* Blog 1 */}
+						<div style={{ marginBottom: "1.5em", display: "flex", gap: "1em" }}>
+							<img
+								src="https://avatars.githubusercontent.com/u/93050582?v=4" // Replace with your project image URL
+								alt="Blog 1"
+								style={{
+									width: "4.5em",
+									height: "5em",
+									borderRadius: "0.25em",
+									objectFit: "cover",
+								}}
+							/>
+							<div>
+								<h3 style={{ textDecoration: "none", color: "var(--prm)" }}> Blog 1 </h3>
+								<h6 style={{ textDecoration: "none", color: "var(--scd)", marginBottom: "0.5em" }}> A Subtitle Of The Blog. </h6>
+								<p className="text" style={{ fontSize: "0.65em" }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dignissim velit eu nulla feugiat, in gravida nunc faucibus. Donec eu fringilla lectus, non vulputate diam
+									<a href="#" className="link">...Read More </a></p>
+							</div>
+						</div>
+
+						{/* Blog 2 */}
+						<hr style={{ width: "90%", borderTop: "1px dashed var(--acc)" }} />
+						<div style={{ marginBottom: "1.5em", display: "flex", gap: "1em" }}>
+							<img
+								src="https://avatars.githubusercontent.com/u/93050582?v=4" // Replace with your project image URL
+								alt="Blog 1"
+								style={{
+									width: "4.5em",
+									height: "5em",
+									borderRadius: "0.25em",
+									objectFit: "cover",
+								}}
+							/>
+							<div>
+								<h3 style={{ textDecoration: "none", color: "var(--prm)" }}> Blog 2 </h3>
+								<h6 style={{ textDecoration: "none", color: "var(--scd)", marginBottom: "0.5em" }}> A Subtitle Of The Blog. </h6>
+								<p className="text" style={{ fontSize: "0.65em" }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dignissim velit eu nulla feugiat, in gravida nunc faucibus. Donec eu fringilla lectus, non vulputate diam
+									<a href="#" className="link">...Read More </a></p>
+							</div>
+						</div>
+
+						{/* Blog 3 */}
+						<hr style={{ width: "90%", borderTop: "1px dashed var(--acc)" }} />
+						<div style={{ marginBottom: "1.5em", display: "flex", gap: "1em" }}>
+							<img
+								src="https://avatars.githubusercontent.com/u/93050582?v=4" // Replace with your project image URL
+								alt="Blog 1"
+								style={{
+									width: "4.5em",
+									height: "5em",
+									borderRadius: "0.25em",
+									objectFit: "cover",
+								}}
+							/>
+							<div>
+								<h3 style={{ textDecoration: "none", color: "var(--prm)" }}> Blog 3 </h3>
+								<h6 style={{ textDecoration: "none", color: "var(--scd)", marginBottom: "0.5em" }}>
+									A Subtitle Of The Blog. </h6>
+								<p className="text" style={{ fontSize: "0.65em" }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dignissim velit eu nulla feugiat, in gravida nunc faucibus. Donec eu fringilla lectus, non vulputate diam
+									<a href="#" className="link">...Read More </a></p>
+							</div>
+						</div>
+					</div>
+				</>
+			)}
+		</div>
+	);
+}
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+
+	server: {
+		host: true, // This makes the app accessible to devices on the same network
+		port: 5173 // You can change this to any unused port
+	  },
 	plugins: [
 		react()
 	],
